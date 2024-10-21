a8224d0c77d767c43b274ebe860ea0fc5b1d8027
krlb <krlb@lundbeck.com>
2024-10-21 12:06:25 +0200

    Commit once more - take 2

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,12 +12,12 @@ class BasicTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
         expected_quotes = [
-            "Code is like humor. When you have to explain it, it’s bad.", "author": "Cory House",
-            "Fix the cause, not the symptom.", "author": "Steve Maguire",
-            "Optimism is an occupational hazard of programming.", "author": "Kent Beck",
-            "Simplicity is the soul of efficiency.", "author": "Austin Freeman"
+            "Code is like humor. When you have to explain it, it’s bad. - Cory House",
+            "Fix the cause, not the symptom. - Steve Maguire",
+            "Optimism is an occupational hazard of programming. - Kent Beck",
+            "Simplicity is the soul of efficiency. - Austin Freeman"
         ]
-        
+
         response_text = response.data.decode('utf-8')
         self.assertIn(response_text, expected_quotes)
 
